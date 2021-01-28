a7392d3452bf04c9c6517b41277a3d1a53b936bb
heartalborada_del <heartalborada@foxmail.com>
2021-01-28 23:20:05 +0800

    update

--- bg-music.js
+++ bg-music.js
@@ -1,50 +1,58 @@
     //从文件中加载音乐
     function load_music(){
+      mdui.alert("音乐设置:左下角加号和右上角音乐图标");
+      play_music_tip = new mdui.Tooltip('#music-button',{content: '播放音乐', position: 'left'});
+      //ajax载入文件-开始
       var htmlobj= $.ajax({url:"/music-url.txt",async:false});
       var dataString = htmlobj.responseText;
-      url=dataString.split(";");
-      frequency=get_array(url);
-      random = Math.floor(Math.random() * (frequency - 1) ) + Number(1);
-      num=random;
+      //ajax载入文件-结束
+      url=dataString.split(";");//分割文本
+      frequency=get_array(url)-1;//转换变量类型
+      random = Math.floor(Math.random() * (frequency - 0) ) + Number(0);//随机数
+      num=random;//赋值
       audio.src='https://heartalborada-my.sharepoint.com/personal/admin_heartalborada_onmicrosoft_com/_layouts/52/download.aspx?share='+url[random];//伪随机音乐
-      audio.play();
+      audio.pause()
     }
     //音乐进度显示
-    setInterval("music_speed_display();music_speed_f();",1);
+    //进度条
+    setInterval("music_speed_display();music_speed_f();",1);//重复执行
     function music_speed_display(){
-      var all_time=document.getElementById('all_time')
-      var play_time=document.getElementById('play_time')
-      var progress_bar=document.getElementById('speed')
-      var text=document.getElementById('text_play')
-      duration=Math.round(audio.duration)
-      var currentTime=Math.round(audio.currentTime)
-      percentage=currentTime / duration * 100
-      all_time.innerHTML = "总时长:"+duration+"s";
-      play_time.innerHTML = "播放时长:"+currentTime+"s"
-      text.innerHTML="百分比进度:"+percentage.toFixed(3)+"%"
-      progress_bar.style="width:"+percentage+"%"
-      mdui.updateSpinners()
+      var all_time=document.getElementById('all_time')//总时长
+      var play_time=document.getElementById('play_time')//已播放时长
+      var progress_bar=document.getElementById('speed')//进度条
+      var text=document.getElementById('text_play')//文字显示
+      duration=Math.round(audio.duration)//四舍五入总时长(整数)
+      var currentTime=Math.round(audio.currentTime)//四舍五入已播放时长(整数)
+      percentage=currentTime / duration * 100//转换为百分比
+      all_time.innerHTML = "总时长:"+duration+"s";//输出到HTML
+      play_time.innerHTML = "播放时长:"+currentTime+"s"//输出到HTML
+      text.innerHTML="百分比进度:"+percentage.toFixed(3)+"%"//四舍五入百分比进度(保留到千分位)
+      progress_bar.style="width:"+percentage+"%"//进度条显示
+      mdui.updateSpinners()//刷新进度条
     }
+    //滑块
     function music_speed_f(){
-      var progress_change=document.getElementById('change')
-      progress_change.value=percentage.toFixed(3)
-      mdui.updateSliders()
+      var progress_change=document.getElementById('change')//滑块
+      progress_change.value=percentage.toFixed(3)//四舍五入百分比进度(保留到千分位)
+      mdui.updateSliders()//刷新进度条
     }
-    //音乐进度调整
+    //音乐进度调整+判断音乐是否加载
     function music_speed_change(){
-      clearInterval(music_speed_f);
-      var progress_change=document.getElementById("change")
+      clearInterval(music_speed_f);//结束重复执行(不然会一直弹回到播放的位置)
+      var progress_change=document.getElementById("change")//滑块
       var tag=document.getElementById('all_time')
-      if (tag.innerText=="总时长:NaNs"){
-        alert("音乐还未加载完毕哦,请再等等吧!");
+      //判断音乐是否加载成功
+      if (tag.innerText=="总时长:NaNs"){//否
+        alert("音乐还未加载完毕哦,请再等等吧!");//在页面上方弹出警告窗
       }
+      //是
       else{
-        var value=progress_change.value
-        time=Math.round(value * duration / 100)
-        audio.currentTime=time
+        var value=progress_change.value//获取滑块值
+        time=Math.round(value * duration / 100)//计算拖动到的位置,四舍五入(整数)
+        audio.currentTime=time//变更播放进度
       }
     }
-    //我也忘了这是什么了
+    //转换变量类型?
     function get_array(value){
       var type = typeof value;
       if(type == 'string'){
@@ -56,55 +64,59 @@
               return !!ele || ele == 0;
             }).length;
           }
-          return Object.getOwnPropertyNames(value).length;
+          return Object.getOwnPropertyNames(value).length;//返回值
       }
-      return false;
+      return false;//返回值
     }
     //暂停播放
     function bf(){
-      var button = document.getElementById('music-button');
-      if(audio.paused){                 
-        audio.play();
-        button.innerHTML = "pause";
-      }else{
-        audio.pause();
-        button.innerHTML = "play_arrow";
+      //判断音乐是否播放
+      if(audio.paused){//是
+        audio.play();//播放音乐
+      }else{//否
+        audio.pause();//暂停音乐
       }
     }
-   //页面重载
+    //更改tips
+    function change_tips(id,con,pos){
+      id.open({content: con,position:pos})
+    }
+    //更改按钮图标
+    function button_icon_change(id,icon){
+      var Element=document.getElementById(id)
+      Element.innerHTML = icon
+    }
+    //页面重载
     function reloadPage(){
-      window.location.reload()
+      window.location.reload()//重载
     }
     //背景音乐循环
-    // 下一首
-    function prev(){
-      if(num>1){
-        num = num - 1;
+    function prev(){// 下一首
+      //'num'是否大于1
+      if(num>0){//是
+        num = num - 1;//减1
       }
-      else{
-        num=frequency;
+      else{//否
+        num=frequency;//变为'frequency'(最大值)
       }
-      audio.src = 'https://heartalborada-my.sharepoint.com/personal/admin_heartalborada_onmicrosoft_com/_layouts/52/download.aspx?share=' + url[num];
-      next()
+      audio.src = 'https://heartalborada-my.sharepoint.com/personal/admin_heartalborada_onmicrosoft_com/_layouts/52/download.aspx?share=' + url[num];//更改音乐url
+      audio.play();//播放音乐
     }
-    // 上一首
-    function next(){
-      if(num<frequency){
-        num = num + 1;
+    function next(){// 上一首
+      //'num'是否小于'frequency'
+      if(num<frequency){//是
+        num = num + 1;//加1
       }
-      else{
-        num=1;
+      else{//否
+        num=0;//变为0(最小值)
       }
-      audio.src = 'https://heartalborada-my.sharepoint.com/personal/admin_heartalborada_onmicrosoft_com/_layouts/52/download.aspx?share=' + url[num];
-      audio.play();
+      audio.src = 'https://heartalborada-my.sharepoint.com/personal/admin_heartalborada_onmicrosoft_com/_layouts/52/download.aspx?share=' + url[num];//更改音乐url
+      audio.play();//播放音乐
       }
-      audio.addEventListener('ended',function(){
-      next.onclick();
-    },false);
    //音量调整
     function changeVolume(){
-      var display=document.getElementById('volume-display');
-      var Volume=document.getElementById('volume').value;
-      audio.volume=Volume;
-      display.innerHTML = "当前音量:"+Math.round(Volume * 100)+"%";
+      var display=document.getElementById('volume-display');//音量显示
+      var Volume=document.getElementById('volume').value;//获取滑块值
+      audio.volume=Volume;//调整音量
+      display.innerHTML = "当前音量:"+Math.round(Volume * 100)+"%";//变更显示内容
     }
